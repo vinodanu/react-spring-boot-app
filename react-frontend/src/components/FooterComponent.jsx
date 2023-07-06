68f985a28ab6a849ad4939cbacaa5b760df394a0
vinod <anantavinodkumar@gmail.com>
2023-07-06 10:40:01 +0530

    initial commit

---
+++ react-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FooterComponent = () => {
+    return (
+        <div>
+        <footer className = "footer">
+            <span className="text-muted">All Rights Reserved 2023 @VinodAnantha</span>
+        </footer>
+    </div>
+    )
+}
+
+
+export default FooterComponent
